5b9c52f3e22a5af2696fde085b2e85e8e8d51f22
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2023-04-25 01:18:13 +0300

    Edited textemotion tests

--- tests/unit/pes_digenv/test_TextEmotionApi.py
+++ tests/unit/pes_digenv/test_TextEmotionApi.py
@@ -17,21 +17,21 @@ class TestApi(unittest.TestCase):
         self.assertFalse(self.api.is_toxic('Работаем не покладая рук!'))
 
     def test_get_mat(self):
-        self.assertFalse(self.api.get_mat('Ты дурак'))
-        self.assertTrue(self.api.get_mat('Ты мудила'))
-        self.assertTrue(self.api.get_mat('Хуйня из-под коня'))
-        self.assertFalse(self.api.get_mat('Ты такая милая'))
-        self.assertTrue(self.api.get_mat('хуегномы угрожают бомбардировкой'))
-        self.assertFalse(self.api.get_mat('Казахстан угрожает нам бомбардировкой'))
-        self.assertFalse(self.api.get_mat('Мудоёбище должно нас не заметить. Иначе мы попадём в просак'))
+        self.assertFalse(self.api.get_mat('Ты дурак') == 0)
+        self.assertTrue(self.api.get_mat('Ты мудила') == 1)
+        self.assertTrue(self.api.get_mat('Хуйня из-под коня') == 1)
+        self.assertFalse(self.api.get_mat('Ты такая милая') == 0)
+        self.assertTrue(self.api.get_mat('хуегномы угрожают бомбардировкой') == 1)
+        self.assertFalse(self.api.get_mat('Казахстан угрожает нам бомбардировкой') == 0)
+        self.assertFalse(self.api.get_mat('Мудоёбище должно нас не заметить. Иначе мы попадём в просак') == 1)
         
     def test_emotion(self):
-        self.assertTrue(self.api.emotion('Мне так плохо'))
-        self.assertTrue(self.api.emotion('Завтра выходной!'))
-        self.assertTrue(self.api.emotion('Такая ясная погода!'))
-        self.assertTrue(self.api.emotion('Ты адекватный?!'))
-        self.assertTrue(self.api.emotion('Корабли лавировали-лавировали да не вылавировали'))
-        self.assertTrue(self.api.emotion('Мне так хорошо!'))
+        self.assertTrue(self.api.emotion('Мне так плохо') == 'грусть')
+        self.assertTrue(self.api.emotion('Завтра выходной!') == 'радость')
+        self.assertTrue(self.api.emotion('Такая ясная погода!') == 'радость')
+        self.assertTrue(self.api.emotion('Ты адекватный?!') == 'возмущение')
+        self.assertTrue(self.api.emotion('Корабли лавировали-лавировали да не вылавировали') == 'безэмоциональное')
+        self.assertTrue(self.api.emotion('Мне так хорошо!') =='радость')
         
 
 if __name__ == "__main__":
